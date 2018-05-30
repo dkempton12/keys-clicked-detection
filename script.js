2aa08a8659521dc5bc86a06c3923b800ebca032d
Dan Kempton <kemptondan@gmail.com>
2018-05-30 13:11:58 -0400

    added js logic

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ const winningCode = 'dankempton';
 
 window.addEventListener('keyup', (e) => {
     console.log(e.key);
+    keysClicked.push(e.key);
+    keysClicked.splice(-winningCode.length-1, keysClicked.length - winningCode.length);
+    if (keysClicked.join("").includes(winningCode)) {
+        console.log('YOU WINNNNNNNNN', '🎉');
+        cornify_add();
+    }
+    console.log(keysClicked);
 })
